2c04f5fc596043759e4dd971c5e1b38826011943
nadie <alessq732@gmail.com>
2022-05-14 16:51:05 -0400

    agregando una interfaz de consola para el jugador

--- Program.cs
+++ Program.cs
@@ -21,8 +21,8 @@ var player4 = table.GetPlayerKeys();
 
 bool game_finished = false;
 int current_player=0;
-
-
+int ficha_actual=0;
+IEnumerable<IKey<int>> player_keys = player1;
 while(!game_finished)
 {
 Console.Clear();
@@ -30,34 +30,112 @@ print_player_number(current_player+1);
 switch(current_player)
 {
     case 0:
-       print_player_keys(player1);
+        player_keys=player1;
        break;
 
        case 1:
-       print_player_keys(player2);
+        player_keys=player2;
        break;
-
        case 2:
-       print_player_keys(player3);
-       break;
-
+        player_keys=player3;
+            break;
        case 3:
-       print_player_keys(player4);
+         player_keys=player4;
        break;       
 }
-Marcar_Ficha(2,player1);
-Thread.Sleep(1000);
+print_player_keys(player_keys);
+Marcar_Ficha(ficha_actual,player_keys);
+ImrimirAcciones();
+var reading = Console.ReadKey();
 
 
+int available_choices = get_valid_choices(table,player_keys);
+
+if(available_choices==0)
+{
+    SendMessage("no tienes jugadas disponibles , pasando de turno");
+     current_player=(current_player+1)%4;
+     continue;
 }
+
+if(reading.KeyChar == 'a' ||  reading.KeyChar == 'A')
+{
+    ficha_actual= ((ficha_actual-1)+player_keys.Count())%player_keys.Count();
+}
+if(reading.KeyChar == 's' ||  reading.KeyChar == 'S')
+{
+    ficha_actual= ((ficha_actual+1)+player_keys.Count())%player_keys.Count();
+}
+if(reading.KeyChar == 'j' ||  reading.KeyChar == 'J')
+{    
+    IKey<int> ficha = player_keys.ElementAt(ficha_actual);  
+
+    if(table.IsValid(ficha))
+    {
+        
+        table.PlayKey(ficha);
+         current_player=(current_player+1)%4;
+         if(player_keys.Count()==0)game_finished=true;
+           player_keys = player_keys.Where(elem=>elem!=ficha);
+         continue;
+
+    }else{
+        SendMessage("Jugada no Valida");
+    }
+}
+if(reading.KeyChar == 't' ||  reading.KeyChar == 'T')
+{
+    if(available_choices!=0)
+    {
+        SendMessage("aun quedan jugadas validas");
+        continue;
+    }
+    current_player=(current_player+1)%4;
+}
+Thread.Sleep(1000/60);
+}
+
+
+
+int get_valid_choices(ITable<int> mesa , IEnumerable<IKey<int>> fichas)
+{
+    var jugadas = mesa.ValidKeys();
+    if(jugadas.Count()==0)return 1;
+    return fichas.Where(elem=> jugadas.Contains(elem.GetFace(0)) || jugadas.Contains(elem.GetFace(1))).Count();
+}
+
 
 void Marcar_Ficha(int numb,IEnumerable<IKey<int>> jugador)
 {
     int screen_width = Console.WindowWidth;
 int screen_height = Console.WindowHeight;
-Console.SetCursorPosition((screen_width/2)-((jugador.Count()/2)*5)+(numb*4)+1,screen_height-4);
+Console.SetCursorPosition(((screen_width/2)-((jugador.Count()/2)*5)+(numb*5))+2,screen_height-4);
 Console.Write("*");
 }
+
+void SendMessage(string message)
+{
+
+    int screen_width = Console.WindowWidth;
+int screen_height = Console.WindowHeight;
+Console.SetCursorPosition((screen_width/2)-(message.Length/2),screen_height/2);
+Console.BackgroundColor= ConsoleColor.DarkMagenta;
+Console.ForegroundColor= ConsoleColor.DarkRed;
+Console.WriteLine(message);
+Console.ResetColor();
+Thread.Sleep(3000);
+
+}
+
+
+void ImrimirAcciones(){
+    Console.SetCursorPosition(0,Console.WindowHeight);
+    Console.Write("[A]nterior ficha ");
+     Console.Write("[S]iguiente ficha ");
+     Console.Write("[J]ugar ficha actual ");
+      Console.Write("[T]erminar turno ");
+}
+
 
 void print_player_number(int number)
 {
