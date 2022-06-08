3b7fc74051a0365b1c5a78240cdb229e6b86bfb6
nadie <alessq732@gmail.com>
2022-06-08 05:01:25 -0400

    actualizacion de la grafica por consola

--- GraphicLogic/ScreenPrinter.cs
+++ GraphicLogic/ScreenPrinter.cs
@@ -12,30 +12,43 @@ public class ScreenPrinter:ITableObserver ,IPlayerChangedObserver
          while(true)
         {
             Console.Clear();
-            foreach(var a in game_board)
-            {Console.Write(a);}           
+            PrintTable();
             Thread.Sleep(1000);
         }
     }
     
     public void PrintTable()
     {
-       
+        int width = Console.WindowWidth;
+        int height = Console.WindowHeight;
+        Console.SetCursorPosition(width/2,height/2);
+      Console.WriteLine("la mesa XD");
+        Console.SetCursorPosition(0,0);
     }
+
+
+
     public void PrintPlayer(IPlayer player)
     {
-        Console.WriteLine("turno de :{0}",player.GetIdentifier()); 
+        int width = Console.WindowWidth;
+        int height = Console.WindowHeight;
+        Console.WriteLine("turno de : {0}",player.GetIdentifier()); 
         var keyset = player.GetKeys();
+        Console.SetCursorPosition((width/2)-(keyset.Count()*7)/2,height-2);
         foreach(var a in keyset)
         {
            PrintHorizontalKey(a);           
         }
-       Console.WriteLine(" ");       
+        Console.SetCursorPosition(0,0);
     }
    
-   private void PrintVerticalKey(IKey key)
+   private void PrintVerticalKey(IKey key,bool up=false)
    {
-
+        foreach(var b in key.GetAllFaces()){
+            var coord_ant = Console.GetCursorPosition();
+              Console.Write(b.GetRepresentation());              
+            Console.SetCursorPosition(coord_ant.Item1,coord_ant.Item2+(up?-1:1));
+         }
    }
 
    public void PrintHorizontalKey(IKey key)
@@ -50,7 +63,7 @@ public class ScreenPrinter:ITableObserver ,IPlayerChangedObserver
     public void Update(KeyPlayedEvent eventinfo)
     {
           var key = eventinfo.GetEventData().Item1;
-        var index = eventinfo.GetEventData().Item2;
+        var index = eventinfo.GetEventData().Item2-1;
         if(game_board.All((elem)=>elem.Count()==0))
         {
              foreach(var a in game_board)
