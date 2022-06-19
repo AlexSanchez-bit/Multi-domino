649828a7d08b68ec65a8082d8e16a4363ecd5b72
nadie <alessq732@gmail.com>
2022-06-19 07:56:03 -0400

    mejorada interfaz Grafica y aplicada con el builder

--- Grafica/CustomizationScreen/CustomizationWindow.axaml.cs
+++ Grafica/CustomizationScreen/CustomizationWindow.axaml.cs
@@ -2,10 +2,13 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Controls.Shapes;
+using System.Collections;
+using System.Collections.Generic;
 namespace Grafica
 {
     public partial class CustomizationWindow : Window,ITemporalWindow
-    {      
+    {   
+        private ConfigFile game_file;   
         public CustomizationWindow()
         {
             InitializeComponent();           
@@ -28,6 +31,29 @@ namespace Grafica
         public void GoBack(object obj,RoutedEventArgs args)
         {
            ReturnToOriginal();
+        }
+
+          public void Initialize(object obj,RoutedEventArgs args)
+        {
+           var game_name = this.Get<TextBox>("GameName").Text;
+           var keyset = ((ComboBoxItem)this.Get<ComboBox>("Keyset").SelectedItem).Content.ToString();
+            var board = ((ComboBoxItem?)this.Get<ComboBox>("Board").SelectedItem).Content.ToString();
+             var wincondition = ((ComboBoxItem?)this.Get<ComboBox>("WinCondition").SelectedItem).Content.ToString();
+              var player_selector = ((ComboBoxItem?)this.Get<ComboBox>("PlayerSelector").SelectedItem).Content.ToString();
+               var key_selector = ((ComboBoxItem?)this.Get<ComboBox>("KeySelector").SelectedItem).Content.ToString();
+             string[] configs = new string[]{game_name,keyset,board,wincondition,player_selector,key_selector};            
+            if(keyset=="" || board=="" || wincondition==""|| player_selector=="" || key_selector=="")
+            {
+                    
+            }else
+            {
+               
+                ConfigFile game_config = new ConfigFile(configs);
+                foreach(var a in configs)
+                {
+                    System.Console.WriteLine(a);
+                }
+            }                       
         }
     }
     
